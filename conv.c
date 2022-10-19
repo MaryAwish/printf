b53632e584fb7d69c296e41790b70b18ad6440c8
Mary_Ouma <maryawish54@gmail.com>
2022-10-19 03:38:26 -0700

    Project

---
+++ conv.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * convert - converts number and base to string
+ * @num: input number
+ * @base: input base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: result string'
+ */
+
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buff[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buff[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
